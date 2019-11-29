845a7112363f364b81b1a66419b587c11c98db9b
Tupac Rocher <tupac.rocher@etu.umontpellier.fr>
2019-11-29 13:14:00 +0100

    Correction barre de recherche

--- view/produit/searched.php
+++ view/produit/searched.php
@@ -1,9 +1,15 @@
 <?php
-    foreach ($tab as $p) {
-        $codeP = $p->get('codeProduit');
-        $img_nom =$codeP . '.png';
-        echo '<div class=Produit><a href="index.php?controller=produit&action=read&codeProduit='.rawurlencode($codeP).'"><img src="./Images/'.htmlspecialchars($img_nom).'" alt="Ca fonctionne pas nulos"></a>';
-        echo '<div class=desc>'.htmlspecialchars($p->get('nomProduit')).'<br><a>Seulement  ' . htmlspecialchars($p->get('prixProduit')) . '€</a></div>';
-        echo '</div>';
+    if($tab){
+        foreach ($tab as $p) {
+            $codeP = $p->get('codeProduit');
+            $img_nom =$codeP . '.png';
+            echo '<div class=Produit><a href="index.php?controller=produit&action=read&codeProduit='.rawurlencode($codeP).'"><img src="./Images/'.htmlspecialchars($img_nom).'" alt="Ca fonctionne pas nulos"></a>';
+            echo '<div class=desc>'.htmlspecialchars($p->get('nomProduit')).'<br><a>Seulement  ' . htmlspecialchars($p->get('prixProduit')) . '€</a></div>';
+            echo '</div>';
+        }
     }
+    else{
+        echo '<p>Aucun résultat pour votre recherche';
+    }
+
 ?>
