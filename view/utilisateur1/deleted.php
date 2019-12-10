609d74af0faeb13cc69d9f2530b9d8bf4ef866ad
Tupac Rocher <tupac.rocher@etu.umontpellier.fr>
2019-12-10 15:48:31 +0100

    Salut

--- view/utilisateur1/deleted.php
+++ view/utilisateur1/deleted.php
@@ -1,6 +1,6 @@
 <?php 
     echo '<p> L\'utilisateur est supprimé ! <br>
     Retour vers l\'accueil...</p>
-            <meta http-equiv="refresh" content="3; URL=index.php" />';
+            <meta http-equiv="refresh" content="3;URL=index.php" />';
 
 ?>
